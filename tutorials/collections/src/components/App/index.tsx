5cd64b20cc04b2ced0c4f8a902629ec9c5eefe0d
Paoli <sacha.paoli@student.vinci.be>
2024-10-25 11:48:35 +0200

    new: ex2.3

---
+++ tutorials/collections/src/components/App/index.tsx
@@ -0,0 +1,20 @@
+
+//import pizzaBackground from "./assets/images/pizza.jpg";
+import "./App.css";
+import Header from "../Header";
+import Main from "../Main";
+import Footer from "../Footer";
+
+function App() {
+  return (
+    <div className="page">
+      <Header title="We love Pizza"/>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ tutorials/collections/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+
+//import pizzaBackground from "./assets/images/pizza.jpg";
+import "./Header.css";
+
+
+
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header = (props: HeaderProps) => {
+  return (
+    <header>
+      <h1 className="animate__animated animate__bounce">{props.title}</h1>
+    </header>
+  );
+};
+
+export default Header;
